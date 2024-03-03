dd26bf3d10b496457b45f5ea896ed7f770431bfc
Dave Royal <revad@users.noreply.github.com>
2024-03-03 21:38:09 +0000

    Test userscript

---
+++ shared/close_tab.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Close tab after interval
+// @version     1
+// @include     http://example.org
+// @grant       none
+// ==/UserScript==
+setTimeout(window.close,5000);
