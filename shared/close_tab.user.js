5fb7cd5983f02b89d05c246c54da75df3a46ce8d
Dave Royal <revad@users.noreply.github.com>
2024-11-04 18:00:17 +0000

    Delete shared/close_tab.user.js

--- shared/close_tab.user.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name        Close tab after interval
-// @version     1
-// @include     http://example.org/
-// @include     https://example.org/
-// @grant       none
-// ==/UserScript==
-setTimeout(window.close,5000);
